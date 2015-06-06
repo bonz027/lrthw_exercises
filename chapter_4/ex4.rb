234d15725a530f062fd681419ab1d8195cbefe1e
Omar Castro <ocastro24@yahoo.com>
2015-06-06 15:57:08 -0600

    Added Readme.md updates
    [#95933932]

--- chapter_4/ex4.rb
+++ chapter_4/ex4.rb
@@ -7,15 +7,15 @@ cars_driven = drivers
 carpool_capcity = cars_driven * space_in_a_car
 average_passengers_per_car = passengers / cars_driven
 
-#gives us the total amount of cars available
+# gives us the total amount of cars available
 puts "There are #{cars} cars available"
-#looks at the total drivers and gives us the how many are available.
+# looks at the total drivers and gives us the how many are available.
 puts "There are only #{drivers} drivers available"
-#subtracts the amount of drivers from the amount of cars available.
+# subtracts the amount of drivers from the amount of cars available.
 puts "There will be #{cars_not_driven} empty cars today"
-#multiplies the number of cars times the number of spaces in the cars available.
+# multiplies the number of cars times the number of spaces in the cars.
 puts "We can transport #{carpool_capcity} people today."
-#points to 90, the amount of passengers to transport
+# points to 90, the amount of passengers to transport
 puts "We have #{passengers} to carpool today."
-#this divides the passengers by the amount of cars driven.
+# this divides the passengers by the amount of cars driven.
 puts "We need to put about #{average_passengers_per_car} in each car"
