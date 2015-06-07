4fda455ff594f59898812881d435522fb23262f1
Omar Castro <ocastro24@yahoo.com>
2015-06-07 12:30:59 -0600

    Added answers to Readme.md file and code to the ex6 file
    [#95933936]

---
+++ chapter_6/Readme.md
@@ -0,0 +1,8 @@
+The places where there are strings inside a string is in the w + e sentence, the x and y sentence, and finally the y 
+sentence.
+
+Adding the w + e makes the a longer string because it's adding the variables of both w and e one after the other which 
+completes the sentence as a whole. 
+
+When you change the strings to single quote it does not work because single quotes are used for individual characters 
+and double quotes (from what I understand) is added for multiple string characters. 
---
+++ chapter_6/ex6.rb
@@ -0,0 +1,26 @@
+# gives us the variable for types of people
+types_of_people = 10
+# gives us the first line and enters the types of people variable
+x = "There are #{types_of_people} types of people."
+# gives us the binary variable
+binary = "binary"
+# give us the don't variable
+do_not = "don't"
+# enters the binary variable
+y = "Those who know #{binary} and those who #{do_not}."
+# gives us the two x and y variables
+puts x
+puts y
+# enters both x and y into the code
+puts "I said: #{x}."
+puts "I also said: '#{y}'."
+# adds the hilarious variable into the code
+hilarious = false
+# enters the joke evaluation variable into the code
+joke_evaluation = "Isn't that so funny?! #{hilarious}"
+# enters both w & e into the code
+w = "This is the left side of..."
+e = "A string with a right side."
+
+# this string below adds both strings for w and e into one sentence
+puts w + e
